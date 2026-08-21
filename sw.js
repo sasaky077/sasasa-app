aec73df8b670527910219172030bb474e90970f1
sasaky077 <yaaa07ss@gmail.com>
2026-08-21 21:42:36 +0900

    koko

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *    同じbuildなら次回以降はキャッシュから即返す＝毎回フルDLしない。
  *    buildが変わればCACHE_NAMEごと新規作成され、古いキャッシュはactivate時に破棄される。
  */
-const SW_BUILD = '20260821-ch04-02-unlock-fix-v199';
+const SW_BUILD = '20260821-ninmu-header-match-v200';
 const ASSET_CACHE = `sasaphia-assets-${SW_BUILD}`;
 
 self.addEventListener('install', event => {
