c42a60f77024d344d7acba86dc6907953d7d6ab6
sasaky077 <yaaa07ss@gmail.com>
2026-09-09 00:24:57 +0900

    Add title loading progress UI

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *    同じbuildなら次回以降はキャッシュから即返す＝毎回フルDLしない。
  *    buildが変わればCACHE_NAMEごと新規作成され、古いキャッシュはactivate時に破棄される。
  */
-const SW_BUILD = '20260908-title-startup-asset-gate-v158';
+const SW_BUILD = '20260909-title-loading-progress-v159';
 const ASSET_CACHE = `sasaphia-assets-${SW_BUILD}`;
 
 self.addEventListener('install', event => {
